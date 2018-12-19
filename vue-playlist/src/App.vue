8f6f96ab4d1226d76457d06518dd962a0781e009
Jonsonice <415410795@qq.com>
2018-12-19 20:53:08 +0800

    Vue2.x-实战Demo(组件嵌套)

--- vue-playlist/src/App.vue
+++ vue-playlist/src/App.vue
@@ -1,8 +1,9 @@
 <!-- 1模板 html结构 -->
 <template>
   <div id="app">
-    <h1>{{title}}</h1>
+    <app-header></app-header>
     <users></users>
+    <app-footer></app-footer>
   </div>
 </template>
 
@@ -10,6 +11,8 @@
 <script>
   //局部注册组件
 import Users from './components/Users'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
@@ -19,9 +22,11 @@ export default {
     }
   },
   components:{
-    // "users":Users
     // "p":Users   与系统标签冲突 不可取
-    Users
+    // Users
+    "users":Users,
+    "app-header":Header,
+    "app-footer":Footer
   }
 }
 </script>

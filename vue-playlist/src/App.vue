75fbfd8e070f32cdecb689dc115796066ff4bd61
Jonsonice <415410795@qq.com>
2018-12-19 14:47:44 +0800

    Vue2.x-组件嵌套

---
+++ vue-playlist/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <users></users>
+  </div>
+</template>
+
+<script>
+  //局部注册组件
+import Users from './components/Users'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      title:"这是我的第一个vue脚手架项目"
+    }
+  },
+  components:{
+    // "users":Users
+    // "p":Users   与系统标签冲突 不可取
+    Users
+  }
+}
+</script>
+
+<style>
+
+</style>

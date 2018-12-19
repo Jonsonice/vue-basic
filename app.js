500f9ec6dff905c49863e05262013571b10c122e
Jonsonice <415410795@qq.com>
2018-12-19 09:42:13 +0800

    Vue2.x-双向数据绑定

---
+++ app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        name: '',
+        age: ''
+    },
+    methods: {
+        logName: function(){
+            // console.log('你正在输入名字');
+			this.name = this.$refs.name.value;
+			console.log(this.$refs.name.value);
+        },
+        logAge: function(){
+            console.log('你正在输入年龄');
+        }
+    }
+});
